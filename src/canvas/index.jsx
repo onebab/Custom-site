7ddf605bdea6ff5b6a9ae26cb5adc5ca84dfe873
onebab <onebab1111@gmail.com>
2026-03-05 19:50:37 -0600

    Update index.jsx

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,23 +1,54 @@
 import { Canvas } from '@react-three/fiber'
-import { Center } from '@react-three/drei';
-import Shirt from './Shirt';
-import CameraRig from './CameraRig';
+import { Center, OrbitControls } from '@react-three/drei'
+import Shirt from './Shirt'
+import CameraRig from './CameraRig'
+import { useEffect, useRef } from 'react'
 
 const CanvasModel = () => {
-  return (
-    <Canvas
-      camera={{ position: [0, 0, 0], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-      className="w-full max-w-full h-full transition-all ease-in"
-    >
-      <ambientLight intensity={0.5} />
+  const canvasRef = useRef()
 
-      <CameraRig>
-        <Center>
-          <Shirt />
-        </Center>
-      </CameraRig>
-    </Canvas>
+  useEffect(() => {
+    const handleResize = () => {
+      if (canvasRef.current) {
+        // Force re-render on resize if needed (helps with camera)
+        const camera = canvasRef.current.getContext('webgl').camera
+        if (camera) {
+          camera.updateProjectionMatrix()
+        }
+      }
+    }
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <div className="relative w-full h-[60vh] sm:h-[70vh] lg:h-[80vh] flex-grow overflow-hidden">
+      <Canvas
+        ref={canvasRef}
+        shadows
+        camera={{ position: [0, 0, 4], fov: 35, near: 0.1, far: 100 }}  // wider FOV + farther back = bigger shirt on load
+        gl={{ preserveDrawingBuffer: true, antialias: true }}
+        className="w-full h-full"
+      >
+        <ambientLight intensity={0.6} />
+        <directionalLight intensity={1} position={[5, 10, 5]} castShadow />
+        <CameraRig>
+          <Center top scale={1.2}>  {/* slight global scale boost for initial size */}
+            <Shirt />
+          </Center>
+        </CameraRig>
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          minDistance={1.0}     // allow closer zoom
+          maxDistance={10}
+          minPolarAngle={Math.PI / 12}
+          maxPolarAngle={Math.PI - Math.PI / 12}
+          enableDamping
+          dampingFactor={0.08}
+        />
+      </Canvas>
+    </div>
   )
 }
 
